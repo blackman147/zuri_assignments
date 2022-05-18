6a935b3846989067b8341f899e788eccb0307408
blackman147 <chuksntoka@gmail.com>
2022-05-18 12:24:06 +0100

    another one

---
+++ sum.py
@@ -0,0 +1,10 @@
+# Python program to add two numbers using variables
+
+num1 = 22
+num2 = 10
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of {0} and {1} is {2}" .format(num1, num2, sum))
